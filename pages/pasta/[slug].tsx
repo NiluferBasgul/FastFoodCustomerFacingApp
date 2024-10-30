1669102cc7a5a8ef5fa0797372738c22815ccf67
NiluferBasgul <48926756+NiluferBasgul@users.noreply.github.com>
2024-10-30 14:37:21 +0100

    Update [slug].tsx

--- pages/pasta/[slug].tsx
+++ pages/pasta/[slug].tsx
@@ -19,7 +19,7 @@ export const getStaticPaths = async ({ locales }) => {
     return {
         paths,
         fallback: false
-        // fallback: true
+        // fallback: true uses only when needed
     }
 }
 
